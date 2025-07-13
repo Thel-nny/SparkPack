90a4feb234de69134e449b128b0606ebf2aeb35c
Franchescka Jewel Puljanan <franchesckajewel.puljanan-22@cpu.edu.ph>
2025-07-13 14:20:09 +0800

    feat/account-management-ui

---
+++ sparkpack/src/app/advisor/account/management/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import AdvisorTopNavbar from '@/components/advisor/AdvisorTopNavBar';
+import AdvisorProfileForm from '@/components/account/AdvisorProfileForm';
+
+export default function AdvisorClaimsPage() {
+  return (
+    <>
+      <AdvisorTopNavbar/>
+      <AdvisorProfileForm />
+    </>
+  );
+}
---
+++ sparkpack/src/app/client/account/management/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import AdvisorTopNavbar from '@/components/advisor/AdvisorTopNavBar';
+import ClientProfileForm from '@/components/account/ClientProfileForm';
+
+export default function AdvisorClaimsPage() {
+  return (
+    <>
+      <AdvisorTopNavbar/>
+      <ClientProfileForm />
+    </>
+  );
+}
